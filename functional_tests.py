74f14f193b8d41f0738cfa97ad334b6fe446e4ff
avig <gerbyavi@gmail.com>
2015-09-30 14:18:48 +0300

    Unit Test Resolve Now Finds My Project Root Dir, Finds My View (home_page)

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,22 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):  #1
 
-assert 'Django' in browser.title
+    def setUp(self):  #2
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):  #3
+        self.browser.quit()
+
+    def test_r_u_invited(self):  #4
+        # Gerby goes to his site
+        # to check out its homepage
+        self.browser.get('http://localhost:8000')
+
+        # She notices the page title and header mention to-do lists
+        self.assertIn('ASINS', self.browser.title)  #5
+        self.fail('Finish the test!')  #6
+
+if __name__ == '__main__':  #7
+    unittest.main(warnings='ignore')  #
