7669c0cf35139e883d45f81e7438ead79f5dbe00
avig <gerbyavi@gmail.com>
2015-10-01 16:53:02 +0300

    Model for list Items and associated migration

--- appsins/tests.py
+++ appsins/tests.py
@@ -3,6 +3,7 @@ from django.core.urlresolvers import resolve
 from django.test import TestCase
 from appsins.views import home_page #1
 from django.http import HttpRequest
+from appsins.models import Item
 
 class HomePageTest(TestCase):
 
@@ -29,3 +30,22 @@ class HomePageTest(TestCase):
         )                                   ### expected_html with initiate assignment for the ASIN - only assingnment no post request
                                             ### esponse.content.decode() - actual result from view after mocking post request
         self.assertEqual(response.content.decode(), expected_html)
+
+class ItemModelTest(TestCase):
+
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'The first (ever) list item'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'Item the second'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(second_saved_item.text, 'Item the second')
