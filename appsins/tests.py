4b14410b112dea0dbc53c8a11ce6f89dcdeb2bac
avig <gerbyavi@gmail.com>
2015-10-01 13:02:32 +0300

    Post Request added - Checked by unit test rather than FT, Checked for both failure and success

--- appsins/tests.py
+++ appsins/tests.py
@@ -15,3 +15,17 @@ class HomePageTest(TestCase):
         response = home_page(request)
         expected_html = render_to_string('home.html')
         self.assertEqual(response.content.decode(), expected_html)
+
+    def test_catching_post_asin(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'B00000000' # 3 lines for mocking post request from user
+
+        response = home_page(request) # invoking the view to regard our moicking request - requires additional changes in home page view
+
+        expected_html = render_to_string(
+            'home.html',
+            {'new_item_text':  'B00000000'}
+        )                                   ### expected_html with initiate assignment for the ASIN - only assingnment no post request
+                                            ### esponse.content.decode() - actual result from view after mocking post request
+        self.assertEqual(response.content.decode(), expected_html)
--- appsins/views.py
+++ appsins/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+
 def home_page(request):
-    return render(request, 'home.html')
-# Create your views here.
+    return render(request, 'home.html', {
+        'new_item_text': request.POST.get('item_text', ''), ### catching the post from the request by it's input name="item_text" - not value !!!! 
+    })
